3fdb0def504cdbb41668ba24dc1e523ec28165cc
José Carlos <jose.carpio@tecsup.edu.pe>
2017-04-11 23:11:18 -0500

    Streamming con Socket.io y nodejs
    Streamming con sockect io y nodejs , conectando cámara

--- Streamming/streamming/app.js
+++ Streamming/streamming/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = new express();
-var http = require("socket.io")(http);
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 var Log= require('log'),
 log = new Log('debug')
@@ -8,7 +9,18 @@ log = new Log('debug')
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public"));
 
-app.listen(port,function(){
-log.info('Servidor escuchando en el puerto %s',port)
+app.get('/',function(req,res){
+	res.redirect('index.html');
 });
 
+io.on('connection',function(socket){
+
+socket.on('stream',function(image){
+	socket.broadcast.emit('stream',image);
+});
+});
+
+http.listen(port,function(){
+log.info('Servidor escuchando en el puerto %s',port);  
+});
+	
